c288e9f251f9f27c964f81404b5e568174756a7a
ndurvasula <140.naveen.d@gmail.com>
2018-05-17 19:31:20 -0400

    Add parallelization

--- odmdp/examples/Fishing/main.py
+++ odmdp/examples/Fishing/main.py
@@ -3,6 +3,7 @@ import sys
 import solver, state, space
 import numpy as np
 import pickle
+import threading
 
 def parts(obs):
     arr = []
@@ -11,78 +12,77 @@ def parts(obs):
             arr.append([i])
     return np.array(arr)
 
+def run(typ,rand,sbd,d,tnumber):
+
+    dname = str(typ)+"_"+str(rand)+"_"+str(sbd)+"_"+str(d)+"_"+str(tnumber)
+    pickle.dump(([[[i for i in range(typ)]]],[[typ]]), open(dname+".bounds","wb"))
+    pickle.dump([],open(dname+"true.state","wb"))
+    pickle.dump([],open(dname+"true.reward","wb"))
+
+    MEANS = np.array([i*1.0/(typ-1) for i in range(typ)])
+    if rand:
+        MEANS = np.array([np.random.uniform() for i in range(typ)])
+
+    pickle.dump((typ,MEANS,d), open(dname+"subsolve.dat","wb"))
+
+    env = gym.make('fish-v0')
+    env.initialize(types=typ,sbdepth=sbd,days=d,means=MEANS,discretize=False)
+
+    reward = 0
+    s0 = state.State([parts(env.reset())],dname)
+    sol = solver.Solver(s0,1,1.02,dname)
+    for i in range(d):
+        a = sol.step()
+        s, r, done, _ = env.step(a)
+        sol.update([parts(s)])
+        reward += r
+        #input("Continue:")
+        S = pickle.load(open(dname+"true.state","rb"))
+        R= pickle.load(open(dname+"true.reward","rb"))
+        S.append(s)
+        R.append(r)
+        pickle.dump(S,open(dname+"true.state","wb"))
+        pickle.dump(R,open(dname+"true.reward","wb"))
+        
+        print("ACTUAL FISH:",s)
+        print("ACTUAL REWARD",r)
+     
+    print("FINAL REWARD: ",reward)
+    pickle.dump(reward,open(dname+".final","wb"))
+
 def experiment(exp):
-    if exp <= 240:
+    if exp <= 15:
         typ = 5
-    elif exp <= 480:
+    elif exp <= 30:
         typ = 10
-        exp -= 240
+        exp -= 15
     else:
         typ = 2
-        exp -= 480
-
-    if exp <= 120:
-        rand = False
-    else:
-        rand = True
-        exp -= 120
-
-    if exp <= 60:
-        sbd = False
-    else:
-        sbd = True
-        exp -= 60
-
-    if exp <= 20:
+        exp -= 30
+        
+    if exp <= 5:
         d = 75
-    elif exp <= 40:
+    elif exp <= 10:
         d = 365
-        exp -= 20
+        exp -= 5
     else:
         d = 20
-        exp -= 40
+        exp -= 10
 
-    return typ, rand, sbd, d, exp
+    return typ, d, exp
 
 exp = int(sys.argv[1])
-typ,rand,sbd,d,tnumber = experiment(exp)
+typ,d,tp = experiment(exp)
+threads = []
+for rand in range(2):
+    for sbd in range(2):
+        for tnumber in range(5*(tp-1)+1, 5*(tp-1)+5):
+            print((typ,rand,sbd,d,tnumber,))
+            thr = threading.Thread(target=run, args = (typ,rand,sbd,d,tnumber,))
+            threads.append(thr)
+            thr.start()
 
 
-dname = str(typ)+"_"+str(rand)+"_"+str(sbd)+"_"+str(d)+"_"+str(tnumber)
-pickle.dump(([[[i for i in range(typ)]]],[[typ]]), open(dname+".bounds","wb"))
-pickle.dump([],open(dname+"true.state","wb"))
-pickle.dump([],open(dname+"true.reward","wb"))
-
-MEANS = np.array([i*1.0/(typ-1) for i in range(typ)])
-if rand:
-    MEANS = np.array([np.random.uniform() for i in range(typ)])
-
-pickle.dump((typ,MEANS,d), open(dname+"subsolve.dat","wb"))
-
-env = gym.make('fish-v0')
-env.initialize(types=typ,sbdepth=sbd,days=d,means=MEANS)
-
-reward = 0
-s0 = state.State([parts(env.reset())],dname)
-sol = solver.Solver(s0,.95,1.02,dname)
-for i in range(365):
-    a = sol.step()
-    s, r, done, _ = env.step(a)
-    sol.update([parts(s)])
-    reward += r
-    #input("Continue:")
-    S = pickle.load(open(dname+"true.state","rb"))
-    R= pickle.load(open(dname+"true.reward","rb"))
-    S.append(s)
-    R.append(r)
-    pickle.dump(S,open(dname+"true.state","wb"))
-    pickle.dump(R,open(dname+"true.reward","wb"))
-    
-    print("ACTUAL FISH:",s)
-    print("ACTUAL REWARD",r)
- 
-print("FINAL REWARD: ",reward)
-pickle.dump(reward,open(dname+".final","wb"))
 
 
 
